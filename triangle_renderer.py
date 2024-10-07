e543c042f6404ac065be3021726f3c2289f9168f
Storm <storm.minecrafter.sss@gmail.com>
2024-10-06 22:27:06 -0400

    added depth

--- triangle_renderer.py
+++ triangle_renderer.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def create_coordinates(width=1000,height=1000,left_bound=-2,right_bound=2,lower_bound=-2,upper_bound=2):
+def create_coordinates(width=1000,height=1000,left_bound=-2,right_bound=2,lower_bound=-2,upper_bound=2,render_depth=100):
     pixel_coordinates=np.stack((np.meshgrid(np.arange(left_bound,right_bound,(right_bound-left_bound)/width),np.arange(upper_bound,lower_bound,(lower_bound-upper_bound)/height))),axis=-1)
 
     # contains x and y values for each pixel
@@ -10,6 +10,9 @@ def create_coordinates(width=1000,height=1000,left_bound=-2,right_bound=2,lower_
 
     # here we set up our screen, it contains the RGB values of our image, along with the depth of the current pixel
     screen=np.zeros((height,width,4))
+
+    # here we set maximum depth
+    screen[...,3]=render_depth
 
     return screen,x_values,y_values
 
@@ -26,11 +29,20 @@ def plot_triangle(triangle,screen,x_values,y_values):
     weight_1=(x_values*(y[2]-y[0])-y_values*(x[2]-x[0])-x[0]*(y[2]-y[0])+y[0]*(x[2]-x[0]))/((x[1]-x[0])*(y[2]-y[0])-(x[2]-x[0])*(y[1]-y[0]))
     weight_2=(x_values-x[0]-weight_1*(x[1]-x[0]))/(x[2]-x[0])
 
+    depth_of_pixel=1#plug in equation from desmos
+
     # this filters the screen to only affect values whose x and y are in a triangle, then add the rgb value
     # improve lower command to not draw triangles on top of each other
-    screen[(weight_1>=0)&(weight_2>=0)&(weight_1+weight_2<=1)]+=[*triangle[3],1]
+    color=triangle[3]
+    screen[(weight_1>=0)&(weight_2>=0)&(weight_1+weight_2<=1)&(depth_of_pixel<screen[...,3])]=[*color,depth_of_pixel]
 
 def show_screen(screen,delay=0):
+    # get rid of whatever was there, apparently not the depth
+    screen=screen[:,:,:3]
+
+    # make sure 255 is the greatest possible value
+    screen[screen[...,:]>255]=255
+
     cv2.imshow('Two triangles',screen[:,:,:3].astype(np.uint8))
     cv2.waitKey(delay)
 
@@ -38,16 +50,10 @@ def main():
     screen,x_values,y_values=create_coordinates()
 
     # triangles contains the points of two triangles along with their colors
-    triangles=np.array([[[-1,0,0],[1,0,0],[0,1,1],[255,0,0]],[[-1,0,0],[1,0,0],[0,-1,1],[0,0,255]]])
+    triangles=np.array([[[-1,-.1,-1],[1,-.1,-1],[0,1,1],[255,0,0]],[[-1,.1,-1],[1,.1,-1],[0,-1,1],[0,0,255]]])
 
     for triangle in triangles:
         plot_triangle(triangle,screen,x_values,y_values)
-
-    # get rid of whatever was there, apparently not the depth
-    screen=screen[:,:,:3]
-
-    # make sure 255 is the greatest possible value
-    screen[screen[...,:]>255]=255
 
     show_screen(screen)
 
