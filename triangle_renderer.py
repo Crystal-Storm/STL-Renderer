f07252a4a735cdcc55ba4e3f0cf26e632055f82c
Storm <storm.minecrafter.sss@gmail.com>
2024-10-07 00:28:46 -0400

    have depth :D

--- triangle_renderer.py
+++ triangle_renderer.py
@@ -21,7 +21,8 @@ def plot_triangle(triangle,screen,x_values,y_values):
 
     # x and y are the x and y values for the triangle points. ex: x[0] is the x value for point 0
     # map triangle locations in 3 dimensions to 2 dimensions
-    x,y=triangle[:3,0]*fov/(triangle[:3,1]+fov),triangle[:3,2]*fov/(triangle[:3,1]+fov)
+    d=triangle[:3,1]+fov
+    x,y=triangle[:3,0]*fov/d,triangle[:3,2]*fov/d
 
     # great video https://www.youtube.com/watch?v=HYAgJN3x4GA that explains what weight_1 and weight_2 are
 
@@ -29,15 +30,26 @@ def plot_triangle(triangle,screen,x_values,y_values):
     weight_1=(x_values*(y[2]-y[0])-y_values*(x[2]-x[0])-x[0]*(y[2]-y[0])+y[0]*(x[2]-x[0]))/((x[1]-x[0])*(y[2]-y[0])-(x[2]-x[0])*(y[1]-y[0]))
     weight_2=(x_values-x[0]-weight_1*(x[1]-x[0]))/(x[2]-x[0])
 
-    depth_of_pixel=1#plug in equation from desmos
+    # a=((d[1]-d[0])/(y[0]-y[1])-(d[1]-d[2])/(y[2]-y[1]))/((x[1]-x[0])/(y[0]-y[1])-(x[1]-x[2])/(y[2]-y[1]))
+    # b=(d[0]-a*x[0]-d[1]+a*x[1])/(y[0]-y[1])
+    # c=d[0]-a*x[0]-b*y[0]
+    matrix_1=np.array([[x[0],y[0],1],[x[1],y[1],1],[x[2],y[2],1]])
+    matrix_2=np.array([[d[0]],[d[1]],[d[2]]])
+    inv_matrix_1=np.linalg.inv(matrix_1)
+    abc=np.dot(inv_matrix_1,matrix_2)
+    a,b,c=abc[0,0],abc[1,0],abc[2,0]
+
+    depth_of_pixel=a*x_values+b*y_values+c
 
     # this filters the screen to only affect values whose x and y are in a triangle, then add the rgb value
     # improve lower command to not draw triangles on top of each other
+    pixels_to_change=(weight_1>=0)&(weight_2>=0)&(weight_1+weight_2<=1)&(depth_of_pixel<screen[...,3])
     color=triangle[3]
-    screen[(weight_1>=0)&(weight_2>=0)&(weight_1+weight_2<=1)&(depth_of_pixel<screen[...,3])]=[*color,depth_of_pixel]
+    screen[pixels_to_change,:3]=color
+    screen[pixels_to_change,3]=depth_of_pixel[pixels_to_change]
 
 def show_screen(screen,delay=0):
-    # get rid of whatever was there, apparently not the depth
+    # get rid of depth for drawing
     screen=screen[:,:,:3]
 
     # make sure 255 is the greatest possible value
